521ab5544f1744ed4bd4fd651d995f286f3cafae
swallows98 <79219483+swallows98@users.noreply.github.com>
2021-04-14 10:22:28 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let circle1 = document.getElementsById("Circle1");
+let answer1 = document.getElementsByTagName("Answers1");
+
+
+
+function changeColor(){
+  circle1.style.backgroundColor="#CAFF5A";
+
+}
+
