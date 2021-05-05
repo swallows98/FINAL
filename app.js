fc1f14359b2d68d4c6601856babc805238c315b6
swallows98 <79219483+swallows98@users.noreply.github.com>
2021-05-05 10:11:13 -0600

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// let circle1 = document.getElementsById("Circle1");
-// let answer1 = document.getElementsByTagName("Answers1");
-// function changeColor(){
-//   circle1.style.backgroundColor="#CAFF5A";
-
-// }
-
